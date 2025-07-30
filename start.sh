8fda6ba0611ee09c4de33addf68007f09e413d54
PedroHViudes <pedro.silva@uenp.edu.br>
2025-07-30 20:20:24 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,15 @@
 # O comando 'set -e' garante que o script irá parar se algum comando falhar.
 set -e
 
+# Limpa o cache de configuração para garantir que as variáveis de ambiente sejam lidas
+php artisan config:clear
+
+# <<< ADICIONE A LINHA ABAIXO >>>
+# Cria o link da pasta 'storage' para tornar as imagens públicas
+php artisan storage:link
+
 # Roda as migrações do banco de dados.
 php artisan migrate --force
 
 # Inicia o servidor do Laravel.
-# O 'exec' no final é uma otimização para que o 'php artisan serve' se torne o processo principal.
 exec php artisan serve --host 0.0.0.0 --port ${PORT}
